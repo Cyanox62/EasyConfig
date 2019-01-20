bfd017b5d38902dcb1bab45ac8e22ad6a4773ff2
Todd <infernogaming621@gmail.com>
2019-01-20 03:08:50 -0500

    initial commit

--- EasyConfig/Form1.Designer.cs
+++ EasyConfig/Form1.Designer.cs
@@ -28,13 +28,108 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.ConfigListBox = new System.Windows.Forms.ListBox();
+			this.OpenConfigButton = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.AddConfigButtom = new System.Windows.Forms.Button();
+			this.RemoveConfigButton = new System.Windows.Forms.Button();
+			this.SaveButton = new System.Windows.Forms.Button();
+			this.ConfigValueTextbox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// ConfigListBox
+			// 
+			this.ConfigListBox.FormattingEnabled = true;
+			this.ConfigListBox.Location = new System.Drawing.Point(9, 96);
+			this.ConfigListBox.Name = "ConfigListBox";
+			this.ConfigListBox.Size = new System.Drawing.Size(287, 303);
+			this.ConfigListBox.Sorted = true;
+			this.ConfigListBox.TabIndex = 0;
+			this.ConfigListBox.SelectedIndexChanged += new System.EventHandler(this.ConfigListBox_SelectedIndexChanged);
+			// 
+			// OpenConfigButton
+			// 
+			this.OpenConfigButton.Location = new System.Drawing.Point(12, 12);
+			this.OpenConfigButton.Name = "OpenConfigButton";
+			this.OpenConfigButton.Size = new System.Drawing.Size(139, 23);
+			this.OpenConfigButton.TabIndex = 1;
+			this.OpenConfigButton.Text = "Open File";
+			this.OpenConfigButton.UseVisualStyleBackColor = true;
+			this.OpenConfigButton.Click += new System.EventHandler(this.OpenConfigButton_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// AddConfigButtom
+			// 
+			this.AddConfigButtom.Location = new System.Drawing.Point(12, 41);
+			this.AddConfigButtom.Name = "AddConfigButtom";
+			this.AddConfigButtom.Size = new System.Drawing.Size(139, 23);
+			this.AddConfigButtom.TabIndex = 2;
+			this.AddConfigButtom.Text = "Add Config";
+			this.AddConfigButtom.UseVisualStyleBackColor = true;
+			this.AddConfigButtom.Click += new System.EventHandler(this.AddConfigButtom_Click);
+			// 
+			// RemoveConfigButton
+			// 
+			this.RemoveConfigButton.Location = new System.Drawing.Point(156, 41);
+			this.RemoveConfigButton.Name = "RemoveConfigButton";
+			this.RemoveConfigButton.Size = new System.Drawing.Size(139, 23);
+			this.RemoveConfigButton.TabIndex = 3;
+			this.RemoveConfigButton.Text = "Remove Selected Config";
+			this.RemoveConfigButton.UseVisualStyleBackColor = true;
+			this.RemoveConfigButton.Click += new System.EventHandler(this.RemoveConfigButton_Click);
+			// 
+			// SaveButton
+			// 
+			this.SaveButton.Location = new System.Drawing.Point(156, 12);
+			this.SaveButton.Name = "SaveButton";
+			this.SaveButton.Size = new System.Drawing.Size(139, 23);
+			this.SaveButton.TabIndex = 5;
+			this.SaveButton.Text = "Save Changes";
+			this.SaveButton.UseVisualStyleBackColor = true;
+			this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+			// 
+			// ConfigValueTextbox
+			// 
+			this.ConfigValueTextbox.Location = new System.Drawing.Point(9, 70);
+			this.ConfigValueTextbox.Name = "ConfigValueTextbox";
+			this.ConfigValueTextbox.Size = new System.Drawing.Size(285, 20);
+			this.ConfigValueTextbox.TabIndex = 6;
+			this.ConfigValueTextbox.TextChanged += new System.EventHandler(this.ConfigValueTextbox_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(312, 413);
+			this.Controls.Add(this.ConfigValueTextbox);
+			this.Controls.Add(this.SaveButton);
+			this.Controls.Add(this.RemoveConfigButton);
+			this.Controls.Add(this.AddConfigButtom);
+			this.Controls.Add(this.OpenConfigButton);
+			this.Controls.Add(this.ConfigListBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "EasyConfig v1.0.0";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox ConfigListBox;
+		private System.Windows.Forms.Button OpenConfigButton;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button AddConfigButtom;
+		private System.Windows.Forms.Button RemoveConfigButton;
+		private System.Windows.Forms.Button SaveButton;
+		private System.Windows.Forms.TextBox ConfigValueTextbox;
 	}
 }
 
--- EasyConfig/Form1.cs
+++ EasyConfig/Form1.cs
@@ -1,20 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic;
 using System.Windows.Forms;
 
 namespace EasyConfig
 {
 	public partial class Form1 : Form
 	{
+		private string cPath = null;
+		private Dictionary<string, string> ConfigCache = new Dictionary<string, string>();
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void OpenConfigButton_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				openFileDialog.InitialDirectory = "c:\\";
+				openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+				openFileDialog.FilterIndex = 2;
+				openFileDialog.RestoreDirectory = true;
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					cPath = openFileDialog.FileName;
+				}
+			}
+			if (cPath == null) MessageBox.Show("Error", "Can't find file");
+
+			foreach (string config in File.ReadAllLines(cPath))
+			{
+				int cIndex = config.IndexOf(':');
+				if (config.Length > 0 && cIndex != -1 && !config.StartsWith("port_queue:") && !config.Contains("#"))
+				{
+					if (!ConfigCache.ContainsKey(config.Substring(0, cIndex)))
+					{
+						ConfigCache.Add(config.Substring(0, cIndex), config.Substring(cIndex + 2));
+						ConfigListBox.Items.Add(config.Substring(0, cIndex));
+					}
+				}
+			}
+		}
+
+		private void AddConfigButtom_Click(object sender, EventArgs e)
+		{
+			string input = Interaction.InputBox("Enter config to add\n\nFormat: config: value", "Config Adder", "", -1, -1);
+			int cIndex = input.IndexOf(':');
+			ConfigListBox.Items.Add(input.Substring(0, cIndex));
+			ConfigCache.Add(input.Substring(0, cIndex), input.Substring(cIndex + 2));
+		}
+
+		private void ConfigListBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (ConfigListBox.SelectedItem != null)
+				ConfigValueTextbox.Text = ConfigCache[ConfigListBox.SelectedItem.ToString()];
+			else
+				ConfigValueTextbox.Text = string.Empty;
+		}
+
+		private void RemoveConfigButton_Click(object sender, EventArgs e)
+		{
+			ConfigCache.Remove(ConfigListBox.SelectedItem.ToString());
+			ConfigListBox.Items.Remove(ConfigListBox.SelectedItem);
+		}
+
+		private void SaveButton_Click(object sender, EventArgs e)
+		{
+			List<string> lines = new List<string>(File.ReadAllLines(cPath));
+			for (int i = 0; i < lines.Count; i++)
+			{
+				int cIndex = lines[i].IndexOf(':');
+				if (lines[i].Length > 0 && cIndex != -1 && !lines[i].StartsWith("port_queue:") && !lines[i].Contains("#"))
+				{
+					if (ConfigCache.ContainsKey(lines[i].Substring(0, cIndex)))
+					{
+						lines[i] = $"{lines[i].Substring(0, cIndex)}: {ConfigCache[lines[i].Substring(0, cIndex)]}";
+					}
+					else
+					{
+						lines.Remove(lines[i]);
+					}
+				}
+			}
+
+			foreach (KeyValuePair<string, string> entry in ConfigCache)
+			{
+				if (!lines.Contains($"{entry.Key}: {entry.Value}"))
+				{
+					lines.Add($"{entry.Key}: {entry.Value}");
+				}
+			}
+			File.WriteAllLines(cPath, lines);
+			MessageBox.Show("All changes saved", "Save Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void ConfigValueTextbox_TextChanged(object sender, EventArgs e)
+		{
+			if (ConfigListBox.SelectedItem != null)
+			{
+				ConfigCache[ConfigListBox.SelectedItem.ToString()] = ConfigValueTextbox.Text;
+			}
 		}
 	}
 }
